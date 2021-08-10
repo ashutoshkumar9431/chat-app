aa50df88fafefef7b19fde63ac9e88cb2fdab0f0
ashutosh <ashutoshkumar99055@gmail.com>
2021-08-10 20:49:56 +0530

    scaffolding of project

---
+++ chat-app/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import 'rsuite/dist/styles/rsuite-default.css';
+import './styles/main.scss'
+
+function App() {
+  return (
+    <div>
+    helo
+    </div>
+  );
+}
+
+export default App;
